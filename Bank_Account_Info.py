45ea9529c1d6f7b54fe4ff9fa398a52279d6c7fc
singhj22 <42975589+singhj22@users.noreply.github.com>
2018-10-17 12:43:14 -0400

    Add files via upload

---
+++ Bank_Account_Info.py
@@ -0,0 +1,20 @@
+'''
+Created on Oct 17, 2018
+
+@author: Jugat Singh
+Bank Account Information
+'''
+name= input("Please enter the first account holder’s name: ")
+account_num= int(input("Please enter the first account number: "))
+account_type= input("Please enter the type of account: ")
+balance= float(input("Please enter the balance: "))
+
+name2= input("\nPlease enter the second account holder’s name: ")
+account_num2= int(input("Please enter the second account number: "))
+account_type2= input("Please enter the type of account: ")
+balance2= float(input("Please enter the balance: "))
+
+name3= input("\nPlease enter the third account holder’s name: ")
+account_num3= int(input("Please enter the third account number: "))
+account_type3= input("Please enter the type of account: ")
+balance3= float(input("Please enter the balance: "))
